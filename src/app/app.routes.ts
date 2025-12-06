f7e89033d0490b6202309c1a93119f3c55a3cafd
Virab Shetkar <virabarunshetkar@gmail.com>
2025-12-06 12:18:32 +0530

    Added route and home-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./onboarding/pages/home-page/home-page').then((c) => c.HomePage),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+];
